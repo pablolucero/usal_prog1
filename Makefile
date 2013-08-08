edf5fb629f0d9654712610661977985695c55c8c
Pablo Lucero <plucero@tekgenesis.com>
2013-08-08 17:13:35 -0300

    add -lncurses to the Makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CFLAGS=-Wall -g
+CFLAGS=-Wall -g -lncurses
 
 all:
 	make ej1 ej2 ej3 ej4 ej5 ej6 ej7 ej8 ej9 ej10
--- ej10.c
+++ ej10.c
@@ -1,16 +1,43 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main()
-{
-	char vec[5]; // 
+// int main()
+// {
+// 	char vec[5]; // 
 	
-	printf("Ingrese 20 caracteres: \n");
-	scanf("%s", vec);
+// 	printf("Ingrese 20 caracteres: \n");
+// 	scanf("%s", vec);
 
-	printf("Usted puso: %s\n", vec);
-	printf("Presione una tecla para salir");
-	getchar();getchar();
-}
+// 	printf("Usted puso: %s\n", vec);
+// 	printf("Presione una tecla para salir");
+// 	getchar();getchar();
+// }
 
 // Escribir en Pseudocódigo y codificar en C un programa que lea 20 caracteres. 
 // Luego de la lectura indicar cuantas "a" se ingresaron, cuantas "e, i, o, u"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#define MAX 3
+
+int main(){
+	int i, vec[MAX];
+	/* 
+       La función initscr(); permite la inicialización de la consola
+     */
+    initscr();
+    erase(); // Limpia la pantalla (consola)
+	for(i=0; i<=MAX; i++)
+   {
+   	         printw("Ingrese un numero: ");
+   	         scanw("%d", &vec[i]);
+   }
+   for(i=0; i<=MAX; i++)
+   	        printw("- %d\n", vec[i]);
+   getch();
+   endwin(); // Cierra la aplicación
+   return 0;
+}
+/*
+  Para compilar:  gcc archivo.c -o archivo -lncurses
+*/
